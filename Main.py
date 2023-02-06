8bc8bc1604246d4a6a4afa5a156f8a5e3fc7440a
patrickmatos9 <81051549+patrickmatos9@users.noreply.github.com>
2023-02-06 15:49:05 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,2 +1,8 @@
-import qrcode
-print('ola mundo')
+from Imagem import *
+
+def main():
+    qr1 = Imagem('https://www.linkedin.com/in/vict%C3%B3ria-vaz-5044b81a5/')
+    qr1.geraQr('#ff0f39', '#ffd0d7')
+
+if __name__ == "__main__":
+    main()
